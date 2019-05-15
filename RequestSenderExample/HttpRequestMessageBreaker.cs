85d0708e87c960e9ae680a9e2b494fe3ce2c0c01
Jacek Stec (JTK) <JTK@kmd.dk>
2019-05-15 15:21:49 +0200

    added client

---
+++ RequestSenderExample/HttpRequestMessageBreaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RequestSenderExample
+{
+    public static class HttpRequestMessageBreaker
+    {
+        public static void BreakHeaderContentType(this HttpRequestMessage request)
+        {
+            request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("multipart/form-data");
+        }
+
+        public static void BreakHeaderContentDisposition(this HttpRequestMessage request)
+        {
+            request.Content.Headers.ContentDisposition = null;
+        }
+    }
+}
---
+++ RequestSenderExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RequestSenderExample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string url = "http://localhost:5000/subscriptions/3f85807d-3597-4d7e-8125-6af924f221d7/digital-post/attachments";
+            var client = new HttpClient();
+
+            while (true)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Please click [T] to send wrong content-type");
+                Console.WriteLine("Please click [D] to send wrong content-disposition");
+                Console.WriteLine("Please click [Esc] key to end...");
+
+                var request = new HttpRequestMessage(HttpMethod.Post, url)
+                {
+                    Content = new MultipartFormDataContent()
+                };
+
+                var key = GetProperKey();
+                switch (key)
+                {
+                    case ConsoleKey.T:
+                        request.BreakHeaderContentType();
+                        break;
+                    case ConsoleKey.D:
+                        request.BreakHeaderContentDisposition();
+                        break;
+                    default:
+                        return;
+                }
+
+                Console.Clear();
+                Console.WriteLine("Sending request...");
+
+                try
+                {
+                    var response = await client.SendAsync(request).ConfigureAwait(false);
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"{response.StatusCode}: {content}");
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static ConsoleKey GetProperKey()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.T:
+                    case ConsoleKey.D:
+                    case ConsoleKey.Escape:
+                        return key.Key;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
